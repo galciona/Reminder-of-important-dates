fe8ed3920b19edc6839b49298e7d8fa5d618f8bd
tanya <galciona@gmail.com>
2017-11-09 22:23:37 +0700

    #20 add upload

---
+++ frontend/src/components/UploadFile.vue
@@ -0,0 +1,63 @@
+<template>
+    <div id="app">
+        <div v-if="!image">
+            <h2>Select an image</h2>
+            <input type="file" @change="onFileChange">
+        </div>
+        <div v-else>
+            <img :src="image"/>
+            <button @click="removeImage">Remove image</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['img'],
+        data() {
+            return {
+                image: ''
+            }
+        },
+        computed: {
+            getImg() {
+                if (this.img) return this.img
+                else return image;
+            }
+        },
+        methods: {
+            onFileChange(e) {
+                var files = e.target.files || e.dataTransfer.files;
+                if (!files.length)
+                    return;
+                this.createImage(files[0]);
+            },
+            createImage(file) {
+                var image = new Image();
+                var reader = new FileReader();
+                var vm = this;
+
+                reader.onload = (e) => {
+                    vm.image = e.target.result;
+                };
+                reader.readAsDataURL(file);
+            },
+            removeImage: function (e) {
+                this.image = '';
+            }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    img {
+        width: 30%;
+        margin: auto;
+        display: block;
+        margin-bottom: 10px;
+    }
+
+    button {
+
+    }
+</style>
