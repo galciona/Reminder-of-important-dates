73d7de49af8d36ee92b815b16e00c3e525e9eb25
tanya <galciona@gmail.com>
2017-11-09 18:20:25 +0700

    changed date created

--- server/configs_db/req _employees.js
+++ server/configs_db/req _employees.js
@@ -24,7 +24,7 @@ module.exports.post = (db, param, f) => {
         email: param.email,
         birthday: param.birthday,
         photo: param.photo,
-        created: new Date(now.getFullYear(), now.getMonth(), now.getDate()).valueOf()
+        created: now.getFullYear() + "-" + now.getMonth() +"-" + now.getDate()
     };
     db.collection('employees').insert(note, (err, result) => {
         processingResult(err, result.ops[0], f);    
