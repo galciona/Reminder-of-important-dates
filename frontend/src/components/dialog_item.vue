8bbe21fcf2de7944ca2cc127417a4be718d394f5
tanya <galciona@gmail.com>
2017-11-09 18:15:31 +0700

    #11 refactoring, added data from API, added save item(PUT), added cancel item(GET)

--- frontend/src/components/dialog_item.vue
+++ frontend/src/components/dialog_item.vue
@@ -1,162 +1,147 @@
 <template>
     <v-layout row justify-center>
         <v-dialog v-model="dialog" fullscreen transition="dialog-bottom-transition" :overlay=false>
-    <v-btn color="cyan" dark slot="activator">{{name_btn}}</v-btn>
-    <v-card>
-        <v-toolbar dark color="cyan">
-            <v-btn icon @click.native="dialog = false" dark>
-                <v-icon>close</v-icon>
-            </v-btn>
-            <v-toolbar-title>{{title}}</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-toolbar-items>
-                <v-btn dark flat @click="this.submit">Save</v-btn>
-            </v-toolbar-items>
-        </v-toolbar>
-        <v-cart class="edit-cart">
-            <form>
-                <v-text-field
-                        label="Surname"
-                        v-model=item.surname
-                        :error-messages="surnameErrors"
-                        :counter="20"
-                        @input="$v.surname.$touch()"
-                        @blur="$v.surname.$touch()"
-                        required
-                ></v-text-field>
-                <v-text-field
-                        label="Name"
-                        v-model = item.name
-                        :error-messages="nameErrors"
-                        :counter="20"
-                        @input="$v.name.$touch()"
-                        @blur="$v.name.$touch()"
-                        required
-                ></v-text-field>
-                <v-text-field
-                        label="Position"
-                        v-model=item.position
-                        :error-messages="positionErrors"
-                        :counter="20"
-                        @input="$v.position.$touch()"
-                        @blur="$v.position.$touch()"
-                        required
-                ></v-text-field>
-                <v-text-field
-                        label="Phone"
-                        v-model=item.phone
-                        :error-messages="phoneErrors"
-                        @input="$v.phone.$touch()"
-                        @blur="$v.phone.$touch()"
-                        required
-                ></v-text-field>
-                <v-text-field
-                        label="E-mail"
-                        v-model=item.mail
-                        :error-messages="emailErrors"
-                        @input="$v.email.$touch()"
-                        @blur="$v.email.$touch()"
-                        required
-                ></v-text-field>
-
-                <v-flex xs11 sm5>
-                    <v-menu
-                            lazy
-                            :close-on-content-click="false"
-                            v-model="menu"
-                            transition="scale-transition"
-                            offset-y
-                            full-width
-                            :nudge-right="40"
-                            max-width="290px"
-                            min-width="290px"
-                    >
+            <v-btn color="cyan" dark slot="activator">{{btn_edit}}</v-btn>
+            <v-card>
+                <v-toolbar dark color="cyan">
+                    <v-btn icon @click.native="dialog = false" dark @click="clickClose">
+                        <v-icon>close</v-icon>
+                    </v-btn>
+                    <v-toolbar-title>{{title}}</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-toolbar-items>
+                        <v-btn dark flat @click="clickSave">{{btn_save}}</v-btn>
+                    </v-toolbar-items>
+                </v-toolbar>
+                <v-card class="edit-cart">
+                    <form>
                         <v-text-field
-                                slot="activator"
-                                label="Birthday"
-                                v-model="item.birthday"
-                                prepend-icon="event"
-                                readonly
-                        ></v-text-field>
-                        <v-date-picker v-model="item.birthday" no-title scrollable actions>
-                            <template scope="{ save, cancel }">
-                                <v-card-actions>
-                                    <v-spacer></v-spacer>
-                                    <v-btn flat color="primary" @click="cancel">Cancel</v-btn>
-                                    <v-btn flat color="primary" @click="save">OK</v-btn>
-                                </v-card-actions>
-                            </template>
-                        </v-date-picker>
-                    </v-menu>
-                </v-flex>
+                                label="Surname"
+                                v-model=item.surname
+                                :counter="20"
 
-            </form>
-        </v-cart>
-    </v-card>
-</v-dialog>
+                                required
+                        ></v-text-field>
+                        <v-text-field
+                                label="Name"
+                                v-model=item.name
+                                :counter="20"
+
+                                required
+                        ></v-text-field>
+                        <v-text-field
+                                label="Position"
+                                v-model=item.position
+                                :counter="20"
+
+                                required
+                        ></v-text-field>
+                        <v-text-field
+                                label="Phone"
+                                v-model=item.phone
+
+                                required
+                        ></v-text-field>
+                        <v-text-field
+                                label="E-mail"
+                                v-model=item.mail
+
+                                required
+                        ></v-text-field>
+
+                        <v-flex xs11 sm5>
+                            <v-menu
+                                    lazy
+                                    :close-on-content-click="false"
+                                    v-model="menu"
+                                    transition="scale-transition"
+                                    offset-y
+                                    full-width
+                                    :nudge-right="40"
+                                    max-width="290px"
+                                    min-width="290px"
+                            >
+                                <v-text-field
+                                        slot="activator"
+                                        label="Birthday"
+                                        v-model="item.birthday"
+                                        prepend-icon="event"
+                                        readonly
+                                ></v-text-field>
+                                <v-date-picker v-model="item.birthday" no-title scrollable actions>
+                                    <template scope="{ save, cancel }">
+                                        <v-card-actions>
+                                            <v-spacer></v-spacer>
+                                            <v-btn flat color="primary" @click="cancel">Cancel</v-btn>
+                                            <v-btn flat color="primary" @click="save">OK</v-btn>
+                                        </v-card-actions>
+                                    </template>
+                                </v-date-picker>
+                            </v-menu>
+                        </v-flex>
+                    </form>
+                </v-card>
+            </v-card>
+        </v-dialog>
     </v-layout>
 </template>
-
-
 <script>
-    import { validationMixin } from 'vuelidate'
-    import { required, maxLength, email } from 'vuelidate/lib/validators'
+    import {validationMixin} from 'vuelidate'
+    import {required, maxLength, email} from 'vuelidate/lib/validators'
     import VDatePicker from "vuetify/es5/components/VDatePicker/VDatePicker";
+    import VTextField from "vuetify/es5/components/VTextField/VTextField";
+
     export default {
-/*        components: {VDatePicker},
-        mixins: [validationMixin],
-        validations: {
-            name: { required, maxLength: maxLength(20) },
-            email: { required, email },
-            birthday: { required}
-        },*/
-        data () {
+        props: ['btn_edit', 'btn_save', 'title', 'item'],
+        components: {VTextField},
+        data() {
             return {
                 menu: false,
-                modal: false,
-
                 dialog: false
             }
         },
         methods: {
-            submit () {
+            clickClose: () => {
+                if (window.DEBUG) console.info("Reverse update");
+                this.$store.dispatch('loadEmployee', this.item._id);
+                if (window.DEBUG) console.log(this.item);
 
-                //this.$v.$touch()){
-                    this.dialog = false;
-                //}
             },
-            clear () {
-                this.$v.$reset()
+            clickSave: () => {
+                if (window.DEBUG) console.info(this.item);
+                let itemForSend = {
+                    _id: this.item._id,
+                    name: this.item.name,
+                    surname: this.item.surname,
+                    position: this.item.position,
+                    phone: this.item.phone,
+                    mail: this.item.mail,
+                    birthday: this.item.birthday,
+                    photo: this.item.photo,
+                    created: this.item.created
+                };
+                this.$store.dispatch('putEmployee', [itemForSend]);
+                //this.$store.dispatch('putEmployee',itemForSend);
+                if (window.DEBUG) console.log(this.item);
+                this.dialog = false;
+            },
+            clear: () => {
                 this.name = ''
-                this.email = ''
-                this.select = null
-                this.checkbox = false
+                this.surname = ''
+                this.position = ''
+                this.phone = ''
+                this.mail = ''
+                this.birthday = '2000-10-10'
+                this.photo = ''
             }
-        },
-/*        computed: {
-            nameErrors () {
-                const errors = []
-                if (!this.$v.name.$dirty) return errors
-                !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
-                !this.$v.name.required && errors.push('Name is required.')
-                return errors
-            },
-            emailErrors () {
-                const errors = []
-                if (!this.$v.email.$dirty) return errors
-                !this.$v.email.email && errors.push('Must be valid e-mail')
-                !this.$v.email.required && errors.push('E-mail is required')
-                return errors
-            }
-        },*/
-        props: ['texts', 'name_btn', 'title', 'item']
+        }
     }
 </script>
 
 <style scoped lang="less">
-    .edit-cart{
+    .edit-cart {
         margin-top: 20px;
-        width:800px;
+        width: 800px;
         left: 30px;
         margin-right: 10px;
         position: fixed;
