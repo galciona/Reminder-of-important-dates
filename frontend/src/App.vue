d5519d926d141e398429fbab679afd320cfcf1f9
tanya <galciona@gmail.com>
2017-11-09 17:56:27 +0700

    #9 Refactoring + main js with checing API

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,75 +1,74 @@
 <template>
-  <v-app>
-    <v-tabs dark fixed centered>
-      <v-toolbar color="cyan">
-        <v-tabs-bar class="cyan" slot="extension">
-          <v-tabs-slider color="yellow"></v-tabs-slider>
+    <v-app>
+        <v-tabs dark fixed centered>
+            <v-toolbar color="cyan">
+                <v-tabs-bar class="cyan" slot="extension">
+                    <v-tabs-slider color="yellow"></v-tabs-slider>
 
-          <v-tabs-item @click = "clickTab(i)"
-                  v-for="i in tabs"
-                  :key="i"
-                  :href="'#tab-' + i"
-          >
-            {{ i }}
-          </v-tabs-item>
-        </v-tabs-bar>
-      </v-toolbar>
-      <v-tabs-items>
-        <v-tabs-content
-                v-for="i in tabs"
-                :key="i"
-                :id="'tab-' + i">
-          <v-card flat >
+                    <v-tabs-item @click="clickTab(i)"
+                                 v-for="i in tabs"
+                                 :key="i"
+                                 :href="'#tab-' + i">
+                        {{ i }}
+                    </v-tabs-item>
+                </v-tabs-bar>
+            </v-toolbar>
+            <v-tabs-items>
+                <v-tabs-content
+                        v-for="i in tabs"
+                        :key="i"
+                        :id="'tab-' + i">
+                    <v-card flat>
+                        <v-card-text v-if="keyTab == tabs[0]">
+                            <items-component
 
-            <v-card-text v-if="keyTab == tabs[0]"
-            >
-              <items-component
-                    :texts = texts
-              ></items-component>
-              <v-card-text>
-                <dialog_item-component
-                        :texts = texts
-                        :name_btn = texts.create
-                        :title = texts.create_new
-                        :item = item
-                ></dialog_item-component>
-              </v-card-text>
-            </v-card-text>
+                            ></items-component>
+                            <v-card-text>
+                                <dialog_item-component
+                                        :btn_edit=texts.btn_create
+                                        :btn_save=texts.btn_save
+                                        :title=texts.title_create_new
+                                        :item=item
+                                ></dialog_item-component>
+                            </v-card-text>
+                        </v-card-text>
 
-            <v-card-text v-else
-            >Settings</v-card-text>
+                        <v-card-text v-else
+                        >Settings
+                        </v-card-text>
 
-          </v-card>
-        </v-tabs-content>
-      </v-tabs-items>
-    </v-tabs>
-  </v-app>
+                    </v-card>
+                </v-tabs-content>
+            </v-tabs-items>
+        </v-tabs>
+    </v-app>
 </template>
 
 <script>
-    import {
-        texts
-    } from './texts.js'
+    import {texts} from './texts.js'
+
+    window.DEBUG = true;
+
     export default {
         data() {
             return {
                 item: {
-                    name:"",
-                    surname:"",
-                    position:"",
-                    phone:"",
-                    mail:"",
+                    name: "",
+                    surname: "",
+                    position: "",
+                    phone: "",
+                    mail: "",
                     birthday: "",
                     photo: ""
                 },
                 dialog: false,
-                texts:texts,
+                texts: texts,
                 keyTab: texts.employees,
-                tabs: [texts.employees, texts.settings]
+                tabs: [texts.employees, texts.settings],
             }
         },
         methods: {
-            clickTab: function (key) {
+            clickTab: (key) => {
                 this.keyTab = key;
             }
         }
